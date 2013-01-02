e21223405c119c37e451d613762e047073247e8d
tUrG0n <piotr.yordanov@gmail.com>
2013-01-03 01:17:55 +0200

    Added stub dashboard request

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,30 @@
  */
 
 exports.routes = function(app) {
+  app.get('/dashboard', function(req, res) {
+    users = [];
+    user = {};
+    user.name = "Piotr Yordanov";
+    user.html_url = "https://github.com/tUrG0n";
+    user.gravatar_id = "5a0a63261ea1f4a8b8204a0a993c3e0a";
+    user.nickname = "tUrG0n";
+    user.blog = "http://piotry.me";
+    user.followers = 20;
+    user.following = 20;
+    user.stars = 500;
+    users.push(user);
+
+    user = {};
+    user.name = "n00b";
+    user.html_url = "https://github.com/tUrG0n";
+    user.gravatar_id = "aa0a63261ea1f4a8b8204a0a993c3e0a";
+    user.nickname = "r2a3";
+    user.blog = "http://r2a3.com";
+    users.push(user);
+
+    res.send(users);
+  });
+
   app.get('/video', function(req, res) {
     res.send("http://www.youtube.com/embed/h7ArUgxtlJs");
   });

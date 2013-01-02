62731547b377699cc904bb629ca38fd94f92dda5
Marc <elmarco@launchpad.(none)>
2013-01-03 01:45:44 +0200

    sending user info to client

--- routes/index.js
+++ routes/index.js
@@ -45,18 +45,17 @@ exports.routes = function(app) {
 
     var options = nconf.get('github');
 
-    console.log(options.client_id + ' ' + options.client_secret);
-    
-    console.log('Getting Lebanese-OSS Users');
     var request = require('request');
     var org_url = "https://api.github.com/orgs/Lebanese-OSS/members"+ "?&per_page=100&client_id=" + options.client_id + "&client_secret=" + options.client_secret;
-    console.log(org_url);
+
+    users = [];
     return request(org_url, function(error, response, body) {
       JSON.parse(body).forEach(function(member){
-        console.log('Getting Information For User: ' + member.login);
         member_url = "https://api.github.com/users/"+ member.login + "?per_page=100&client_id=" + options.client_id + "&client_secret=" + options.client_secret;
         return request(member_url, function(error, response, body) {
-          console.log(body);
+          user = JSON.parse(body);
+          console.log(user);
+          users.push(user);
         });
       })
     });
